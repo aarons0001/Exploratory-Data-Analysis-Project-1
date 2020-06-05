e0ea742d1de60be3869c8d16d363d7b43399d7f8
aarons0001 <64659576+aarons0001@users.noreply.github.com>
2020-06-05 14:38:56 -0700

    Update codebook for plot3.R

--- codebook for plot3.R
+++ codebook for plot3.R
@@ -2,18 +2,18 @@ Codebook for plot3:
 
 #Reads in data from file then subsets data for specified dates:
 
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
+read_in <- data.table::fread(input = "household_power_consumption.txt"
                              , na.strings="?"
 )
 
 # Prevents Scientific Notation
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+read_in[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
 # Making a POSIXct date capable of being filtered and graphed by time of day
-powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+read_in[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 
 # Filter Dates for 2007-02-01 and 2007-02-02
-powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+read_in <- read_in[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot3.png", width=480, height=480)
 
@@ -21,9 +21,9 @@ png("plot3.png", width=480, height=480)
 quartz()
 
 # Plot 3
-plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_2],col="red")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+plot(read_in[, dateTime], read_in[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(read_in[, dateTime], read_in[, Sub_metering_2],col="red")
+lines(read_in[, dateTime], read_in[, Sub_metering_3],col="blue")
 legend("topright"
        , col=c("black","red","blue")
        , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
