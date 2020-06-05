6f49b1d1298c7037ca05f76b276909cf43877803
aarons0001 <64659576+aarons0001@users.noreply.github.com>
2020-06-05 14:42:45 -0700

    Update codebook for plot1.R

--- codebook for plot1.R
+++ codebook for plot1.R
@@ -11,24 +11,24 @@ url<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_con
 > download.file(url, destfile)
 
 ## Reads in data table from file the subsets data for specified dates:
-> powerDT <- data.table::fread(input = "household_power_consumption.txt"
+> read_in <- data.table::fread(input = "household_power_consumption.txt"
                               , na.strings="?")
 
 ##Prevents histogram from printing in scientific notation:
-> powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+> read_in[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
 ##Change Date Column to Date Type:
-> powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+> read_in[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
 ##Filter Dates for 2007-02-01 and 2007-02-02:
-> powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+> read_in <- read_in[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
 ##Opening Quartz:
 quartz()
 
 ##Plot 1:
 > png("plot1.png", width=480, height=480)
-> hist(powerDT[, Global_active_power], main="Global Active Power", 
+> hist(read_in[, Global_active_power], main="Global Active Power", 
 +      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 
