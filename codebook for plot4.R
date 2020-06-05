35e93ebc0ceb6038cd0b3759f96651a8850ebc73
aarons0001 <64659576+aarons0001@users.noreply.github.com>
2020-06-05 14:41:09 -0700

    Update codebook for plot4.R

--- codebook for plot4.R
+++ codebook for plot4.R
@@ -2,23 +2,25 @@ Codebook for plot4:
 
 #Reads in data from file then subsets data for specified dates:
 
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
+read_in <- data.table::fread(input = "household_
+
+_consumption.txt"
                              , na.strings="?"
 )
 
 # Prevents Scientific Notation:
 
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+read_in[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
 
 # Making a POSIXct date capable of being filtered and graphed by time of day
 
-powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+read_in[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 
 
 # Filter Dates for 2007-02-01 and 2007-02-02:
 
-powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+read_in <- read_in[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
 png("plot4.png", width=480, height=480)
 
@@ -36,15 +38,15 @@ par(mfrow=c(2,2))
 # Enter each Plot (1:4):
 
 # Plot 1
-plot(powerDT[, dateTime], powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
+plot(read_in[, dateTime], read_in[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
 
 # Plot 2
-plot(powerDT[, dateTime],powerDT[, Voltage], type="l", xlab="datetime", ylab="Voltage")
+plot(read_in[, dateTime],read_in[, Voltage], type="l", xlab="datetime", ylab="Voltage")
 
 # Plot 3
-plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_2], col="red")
-lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+plot(read_in[, dateTime], read_in[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(read_in[, dateTime], read_in[, Sub_metering_2], col="red")
+lines(read_in[, dateTime], read_in[, Sub_metering_3],col="blue")
 legend("topright", col=c("black","red","blue")
        , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
        , lty=c(1,1)
@@ -52,6 +54,6 @@ legend("topright", col=c("black","red","blue")
        , cex=.5) 
 
 # Plot 4:
-plot(powerDT[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(read_in[, dateTime], read_in[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
 
 
