0e4b090b7025ecac88201a549004c960e8ff317e
aarons0001 <64659576+aarons0001@users.noreply.github.com>
2020-06-05 12:38:59 -0700

    Update codebook for plot1.R

--- codebook for plot1.R
+++ codebook for plot1.R
@@ -1,5 +1,9 @@
 Step 1:
 
+library(data.table)
+library(dplyr)
+library(lubridate)
+
 Saving file to working directory and opening within R:
 
 url<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
